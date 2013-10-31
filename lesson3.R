b30c512962431f193d889a92d88a474bb7bdac32
verbe039 <janverbesselt@gmail.com>
2013-10-31 13:48:48 +0100

    ex 3!

--- lesson3.R
+++ lesson3.R
@@ -1,2 +1,19 @@
 ## Lesson 3
-## Nothing here yet - but it is just a test!!!
+## Leap year function
+
+is.leapyear <- function(y) {
+  if (is.numeric(y)) {
+    w <- "1581 is out of the valid range"
+    out <- ifelse(y < 1582, out <- warning(w),
+           ifelse(y %% 400 == 0, TRUE,
+           ifelse(y %% 100 == 0, FALSE, 
+           ifelse(y %% 4 == 0, TRUE, FALSE))))
+  } else {
+    out <- warning("input is not numeric")
+  }
+  return(out)
+}
+
+is.leapyear(2000)
+is.leapyear("john")
+is.leapyear(1581)
